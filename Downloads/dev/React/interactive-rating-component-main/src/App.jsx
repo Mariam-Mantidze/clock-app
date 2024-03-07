4896aa1c88f1d273d2235373add9d1ffa2d36e70
Mariam <mariam.mantidze1@gmail.com>
2024-03-07 14:08:41 +0400

    initial commit

---
+++ Downloads/dev/React/interactive-rating-component-main/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Rate from "./components/Rate/Rate";
+
+function App() {
+  return (
+    <>
+      <Rate />
+    </>
+  );
+}
+
+export default App;
---
+++ Downloads/dev/React/interactive-rating-component-main/src/components/Rate/Rate.jsx
@@ -0,0 +1,11 @@
+import Star from "/images/icon-star.svg";
+
+export default function Rate() {
+  return (
+    <>
+      <div className="rate-container">
+        <img src={Star} alt="icon of star" />
+      </div>
+    </>
+  );
+}
